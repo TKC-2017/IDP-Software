60e5c26f7aea2bc7d0e43e840e5204ba8ca8575b
annav14 <av502@cam.ac.uk>
2018-11-08 10:12:49 +0000

    libraries and navigation update

--- Navigation/Navigation.ino
+++ Navigation/Navigation.ino
@@ -1,3 +1,67 @@
+// motor set up
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *myMotor_left = AFMS.getMotor(1), *myMotor_right = AFMS.getMotor(2), 
+*myMotor_front1 = AFMS.getMotor(3), *myMotor_front2 = AFMS.getMotor(4);
+
+// compass set up
+#include <Adafruit_LSM303.h>
+#include <Adafruit_LSM303_U.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+
+// motor control with variale speed
+
+void forward(int speed) {
+  myMotor_left->setSpeed(speed);
+  myMotor_left->run(FORWARD);
+  myMotor_right->setSpeed(speed);
+  myMotor_right->run(FORWARD);
+}
+
+void backward(int speed) {
+  myMotor_left->setSpeed(speed);
+  myMotor_left->run(BACKWARD);
+  myMotor_right->setSpeed(speed);
+  myMotor_right->run(BACKWARD);
+}
+
+void left() {
+  new_heading = heading - 90
+  while (heading > new_heading) {
+    myMotor_left->setSpeed(100);
+    myMotor_left->run(FORWARD);
+    myMotor_right->setSpeed(100);
+    myMotor_right->run(BACKWARD);
+}}
+
+void right() {
+  new_heading = heading + 90
+  while (heading < new_heading) {
+    myMotor_left->setSpeed(100);
+    myMotor_left->run(BACKWARD);
+    myMotor_right->setSpeed(100);
+    myMotor_right->run(FORWARD);
+}}
+
+void stops() {
+  myMotor_left->setSpeed(0);
+  myMotor_left->run(FORWARD);
+  myMotor_right->setSpeed(0);
+  myMotor_right->run(BACKWARD);
+}
+
+void collector() {
+  myMotor_front1->setSpeed(100);
+  myMotor_front1->run(FORWARD);
+  myMotor_front2->setSpeed(100);
+  myMotor_front2->run(FORWARD);
+  
+}
+
+
 void setup() {
   // put your setup code here, to run once:
 
